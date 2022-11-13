b20518ff1277f8642916cea494a335299e456d34
npakypop <107267029+npakypop@users.noreply.github.com>
2022-11-13 20:08:09 +0000

    Deploying to gh-pages from @ npakypop/goit-js-hw-09@77251bc2b934c370ce3300de1bc97792536aa130 🚀

---
+++ 01-color-switcher.ecd58b84.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){a=setInterval((function(){r.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(a),e.setAttribute("disabled",!0),t.removeAttribute("disabled")}));var a=null;e.setAttribute("disabled",!0)}();
+//# sourceMappingURL=01-color-switcher.ecd58b84.js.map
